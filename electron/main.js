234267dcd4bc45960a68985b4f3275807edf6d5c
Serhii Pokrova <psyx90@gmail.com>
2025-04-16 22:00:40 -0700

    Enhance assets, add icons

--- electron/main.js
+++ electron/main.js
@@ -8,6 +8,7 @@ function createWindow() {
   const win = new BrowserWindow({
     width: 1200,
     height: 800,
+    icon: path.join(__dirname, '..', 'assets', 'icon.png'),
     webPreferences: {
       preload: path.join(__dirname, 'preload.js'), // optional, make sure it exists
       contextIsolation: true,
